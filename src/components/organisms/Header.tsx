3b4c0fc961ad7a276bc9cfb587fe0576601b4440
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-06-22 23:47:15 +0900

    新規登録/ログインクリックでモーダルを表示

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,12 +1,44 @@
-import { Flex, Heading } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FC, memo, useState, ChangeEvent } from "react";
 import { HeaderButton } from "../atoms/HeaderButton";
-import { useOnClickButton } from "../../hooks/useOnClickButton";
 import { useOnClickTitle } from "../../hooks/useOnClickTitle";
 
 export const Header: FC = memo(() => {
-  const { onClickRegister, onClickLogin } = useOnClickButton();
   const { onClickTitle } = useOnClickTitle();
+  const {
+    isOpen: isOpenRegister,
+    onOpen: onOpenRegister,
+    onClose: onCloseRegister,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenLogin,
+    onOpen: onOpenLogin,
+    onClose: onCloseLogin,
+  } = useDisclosure();
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.target.value);
+
   return (
     <>
       <Flex
@@ -26,9 +58,53 @@ export const Header: FC = memo(() => {
         >
           ECサイト
         </Heading>
-        <HeaderButton text={"新規登録"} onClick={onClickRegister} />
-        <HeaderButton text={"ログイン"} onClick={onClickLogin} />
+        <HeaderButton text={"新規登録"} onClick={onOpenRegister} />
+        <HeaderButton text={"ログイン"} onClick={onOpenLogin} />
       </Flex>
+
+      <Modal isOpen={isOpenRegister} onClose={onCloseRegister}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>新規登録</ModalHeader>
+            <ModalCloseButton></ModalCloseButton>
+            <ModalBody mx={4}>
+              <Stack spacing={4}>
+                <FormControl>
+                  <FormLabel>ユーザー名</FormLabel>
+                  <Input value={username} onChange={onChangeUserName} />
+                  <FormLabel>パスワード</FormLabel>
+                  <Input value={password} onChange={onChangePassword} />
+                </FormControl>
+              </Stack>
+            </ModalBody>
+            <ModalFooter>
+              <Button>登録</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+
+      <Modal isOpen={isOpenLogin} onClose={onCloseLogin}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>ログイン</ModalHeader>
+            <ModalCloseButton></ModalCloseButton>
+            <ModalBody mx={4}>
+              <Stack spacing={4}>
+                <FormControl>
+                  <FormLabel>ユーザー名</FormLabel>
+                  <Input value={username} onChange={onChangeUserName} />
+                  <FormLabel>パスワード</FormLabel>
+                  <Input value={password} onChange={onChangePassword} />
+                </FormControl>
+              </Stack>
+            </ModalBody>
+            <ModalFooter>
+              <Button>ログイン</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
     </>
   );
 });
